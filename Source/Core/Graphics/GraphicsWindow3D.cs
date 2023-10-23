536a2352ae7936b3863980bca15e1e1a8105218f
Thiago <thiagomvas@gmail.com>
2023-10-23 09:23:45 -0300

    Improved Controls

--- Source/Core/Graphics/GraphicsWindow3D.cs
+++ Source/Core/Graphics/GraphicsWindow3D.cs
@@ -18,25 +18,27 @@ namespace GameEngineProject.Source.Core.Graphics
             SetTargetFPS(120);
             HideCursor();
             DisableCursor();
-
             while (!WindowShouldClose())
             {
                 // Update
                 // You can add your game logic here.
                 CallOnRedraw();
-                UpdateCamera(ref Engine.Camera.Camera3D, CameraMode.CAMERA_FIRST_PERSON);
 
-                
-                if (IsKeyDown(KeyboardKey.KEY_J))
-                {
-                    cameraObject.Camera3D.position += Vector3.UnitY * Time.DeltaTime;
-                    cameraObject.Camera3D.target += Vector3.UnitY * Time.DeltaTime;
-                }
+                Vector3 movement = new Vector3((IsKeyDown(KeyboardKey.KEY_W) || IsKeyDown(KeyboardKey.KEY_UP) ? 1 : 0) * 0.1f -      // Move forward-backward
+                                (IsKeyDown(KeyboardKey.KEY_S) || IsKeyDown(KeyboardKey.KEY_DOWN) ? 1 : 0) * 0.1f,
+                                (IsKeyDown(KeyboardKey.KEY_D) || IsKeyDown(KeyboardKey.KEY_RIGHT) ? 1 : 0) * 0.1f -   // Move right-left
+                                (IsKeyDown(KeyboardKey.KEY_A) || IsKeyDown(KeyboardKey.KEY_LEFT) ? 1 : 0) * 0.1f,
+                                0.0f);
+                Vector3 rotation = new(GetMouseDelta().X * 0.05f,                            // Rotation: yaw
+                                       GetMouseDelta().Y * 0.05f,                            // Rotation: pitch
+                                       0.0f);                                             // Rotation: roll
+
+                UpdateCameraPro(ref Engine.Camera.Camera3D,
+                                movement,
+                                rotation,
+                                GetMouseWheelMove() * 2.0f);
 
 
-                Engine.Camera.Camera3D.position = Engine.p1.gameObject.Transform.Position;
-                Engine.p1.gameObject.Transform.Rotation = MathExtended.LookAtRotation(Engine.p1.gameObject.Transform.Position, Engine.Camera.Camera3D.target, Vector3.UnitY);
-                // Draw
                 BeginDrawing();
                 {
                     ClearBackground(Color.RAYWHITE);
@@ -44,16 +46,16 @@ namespace GameEngineProject.Source.Core.Graphics
                     // Set camera mode and update it
 
                     // Draw a 3D cube
-                    BeginMode3D(cameraObject.Camera3D);
+                    BeginMode3D(Engine.Camera.Camera3D);
                     DrawSphere(new Vector3(25, 0, 0), 5, Color.RED);
                     DrawSphere(new Vector3(-25, 0, 0), 5, Color.RED);
                     DrawSphere(new Vector3(0, 25, 0), 5, Color.BLUE);
                     DrawSphere(new Vector3(0, -25, 0), 5, Color.BLUE);
                     DrawSphere(new Vector3(0, 0, 25), 5, Color.GREEN);
                     DrawSphere(new Vector3(0, 0, -25), 5, Color.GREEN);
-                    DrawSphere(Engine.p1.gameObject.Transform.Position + Engine.p1.gameObject.Transform.Forward * 5, 0.1f, Color.YELLOW);
+                    DrawSphere(Engine.Camera.Camera3D.target, 0.1f, Color.YELLOW);
                     EndMode3D();
-                    DrawText($"Player Pos: {Engine.p1.gameObject.Transform.Position}\n Camera Pos: {Engine.Camera.Camera3D.position}\n Rotation: {Engine.p1.gameObject.Transform.Rotation}\n {Engine.p1.gameObject.Transform.Position + Engine.p1.gameObject.Transform.Forward * 5}", 20, 10, 10, Color.GREEN);
+                    DrawText($"Player Pos: {Engine.p1.gameObject.Transform.Position}\n Camera Pos: {Engine.Camera.Camera3D.position}\n Rotation: {Engine.p1.gameObject.Transform.Rotation}\n {Engine.p1.gameObject.Transform.Position + Engine.p1.gameObject.Transform.Forward * 5}\n Camera Target {Engine.Camera.Camera3D.target}", 20, 10, 10, Color.GREEN);
                     DrawFPS(10, 10);
 
                     // End the drawing
