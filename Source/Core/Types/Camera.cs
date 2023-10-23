d75a1df84e2d43ff2c5b6dd0bb2ab7ea4d32f91e
Thiago <thiagomvas@gmail.com>
2023-10-23 10:24:17 -0300

    Introduced Camera.Forward and Camera.Right

--- Source/Core/Types/Camera.cs
+++ Source/Core/Types/Camera.cs
@@ -1,6 +1,7 @@
 using GameEngineProject.Source.Core.Utils;
 using GameEngineProject.Source.Entities;
 using Raylib_cs;
+using System.Numerics;
 
 namespace GameEngineProject.Source.Core.Types
 {
@@ -13,6 +14,53 @@ namespace GameEngineProject.Source.Core.Types
         /// </summary>
         public Camera2D Camera2D = new();
         public Camera3D Camera3D = new();
+
+        /// <summary>
+        /// Gets the forward vector of the object's orientation.
+        /// </summary>
+        /// <remarks>
+        /// For a 2D camera, this property returns the forward vector of the object's transformation. For a 3D camera, it returns the normalized vector pointing from the camera position to the camera target.
+        /// </remarks>
+        public Vector3 Forward
+        {
+            get
+            {
+                if (Type == RenderType.Camera2D)
+                {
+                    return Transform.Forward;
+                }
+                else
+                {
+                    return Vector3.Normalize(Camera3D.target - Camera3D.position);
+                }
+            }
+        }
+        /// <summary>
+        /// Gets the right vector of the object's orientation.
+        /// </summary>
+        /// <remarks>
+        /// For a 2D camera, this property is not implemented and throws a <see cref="System.NotImplementedException"/>.
+        /// For a 3D camera, it returns the vector that is rotated 90 degrees to the right from the forward vector.
+        /// </remarks>
+        public Vector3 Right
+        {
+            get
+            {
+                if (Type == RenderType.Camera2D)
+                {
+                    throw new NotImplementedException();
+                }
+                else
+                {
+                    float radians = (float)Math.PI / 2;
+                    Quaternion rotationQuaternion = Quaternion.CreateFromAxisAngle(Camera3D.up, radians);
+                    Vector3 f = Forward;
+                    Vector3 forwardOnXZ = Vector3.Normalize(new (f.X, 0, f.Z));
+                    Vector3 dir = Vector3.Transform(forwardOnXZ, rotationQuaternion);
+                    return Vector3.Normalize(dir);
+                }
+            }
+        }
 
         public Camera(RenderType type)
         {
@@ -31,10 +79,17 @@ namespace GameEngineProject.Source.Core.Types
         /// </summary>
         public void UpdateCameraPosition(object? sender, TransformPositionUpdatedEventArgs e)
         {
-            Camera2D.target = Conversions.XYFromVector3(Transform.Position);
-            Camera2D.offset = new(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2);
-            Camera3D.target = Transform.Position + Transform.Forward;
-            Camera3D.position = Transform.Position;
+            if(Type == RenderType.Camera2D)
+            {
+                Camera2D.target = Conversions.XYFromVector3(Transform.Position);
+                Camera2D.offset = new(Raylib.GetScreenWidth() / 2, Raylib.GetScreenHeight() / 2);
+
+            }
+            else
+            {
+                Camera3D.target = Transform.Position + Transform.Forward;
+                Camera3D.position = Transform.Position;
+            }
         }
 
 
