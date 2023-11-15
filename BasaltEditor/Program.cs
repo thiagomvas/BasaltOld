595a19cf463547e084095c3699adc58ac27fd7b9
Thiago <thiagomvas@gmail.com>
2023-11-15 09:26:32 -0300

    Created a separate project for the editor

---
+++ BasaltEditor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasaltEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Basalt Editor!");
+        }
+    }
+}
