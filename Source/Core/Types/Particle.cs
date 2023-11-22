9fde021efe64287c05610cef1cb90d8d80b5ea28
Thiago <thiagomvas@gmail.com>
2023-11-22 20:31:52 -0300

    Introduced Particle struct to be used by the particle system

---
+++ Source/Core/Types/Particle.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Basalt.Source.Core.Types
+{
+    /// <summary>
+    /// Used by the ParticleSystem component to keep track of GameObject lifetimes.
+    /// </summary>
+    public struct Particle
+    {
+        /// <summary>
+        /// The time this particle was last reset since its instantiation.
+        /// </summary>
+        public TimeOnly resetAt = TimeOnly.FromDateTime(DateTime.Now);
+
+        /// <summary>
+        /// The reference to this particle's object.
+        /// </summary>
+        public GameObject Object;
+
+        public Particle(GameObject @object) : this()
+        {
+            Object = @object;
+        }
+    }
+}
