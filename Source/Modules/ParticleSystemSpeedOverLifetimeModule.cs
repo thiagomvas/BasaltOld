69a3b4c0f14c19590be961071c07b55155509aa7
Thiago <thiagomvas@gmail.com>
2023-11-30 07:46:25 -0300

    Add TMath Nuget Package and replace Easings

--- Source/Modules/ParticleSystemSpeedOverLifetimeModule.cs
+++ Source/Modules/ParticleSystemSpeedOverLifetimeModule.cs
@@ -1,22 +1,23 @@
 using Basalt.Source.Core.Types;
 using Basalt.Source.Core.Utils;
 using Basalt.Source.Interfaces;
+using TMath;
 
 namespace Basalt.Source.Modules
 {
     public class ParticleSystemSpeedOverLifetimeModule : IParticleSystemModule
     {
-        public Easings.EasingType Easing = Easings.EasingType.InQuad;
+        public TEasings.EasingType Easing = TEasings.EasingType.InQuad;
         public float StartSpeed = 50;
         public float EndSpeed = 0;
         public void Initialize(Particle particle)
         {
-            
+
         }
 
         public void Update(Particle particle)
         {
-            float speed = Easings.GetEasing(Easing,particle.ElapsedSinceReset / particle.Lifetime) *  MathF.Abs(StartSpeed - EndSpeed);
+            float speed = TEasings.GetEasing(Easing,particle.ElapsedSinceReset / particle.Lifetime) *  MathF.Abs(StartSpeed - EndSpeed);
             particle.Object.Transform.Position += particle.Object.Transform.Forward * speed * Time.DeltaTime;
         }
         
